bc20f4ad9aa6deb9e16c0c886aa83d8e92893a2a
jcgueriaud1 <jean-christophe@vaadin.com>
2025-09-24 16:03:41 +0300

    Vaadin notification

--- dramafinder-demo/src/test/java/org/vaadin/jchristophe/DialogViewIT.java
+++ dramafinder-demo/src/test/java/org/vaadin/jchristophe/DialogViewIT.java
@@ -97,7 +97,6 @@ public class DialogViewIT extends SpringPlaywrightIT implements HasTestView {
         dialog.assertClosed();
     }
 
-
     @Test
     public void testHasClass() {
         ButtonElement openDialogButton = ButtonElement.getByText(page, "Open dialog with header component");
---
+++ dramafinder/src/main/java/org/vaadin/dramafinder/element/NotificationElement.java
@@ -0,0 +1,37 @@
+package org.vaadin.dramafinder.element;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import org.vaadin.dramafinder.element.shared.HasStyleElement;
+import org.vaadin.dramafinder.element.shared.HasThemeElement;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class NotificationElement extends VaadinElement implements HasThemeElement, HasStyleElement {
+
+    public static final String FIELD_TAG_NAME = "vaadin-notification-card";
+
+    public NotificationElement(Page page) {
+        super(page.locator(FIELD_TAG_NAME));
+    }
+
+    public NotificationElement(Locator locator) {
+        super(locator);
+    }
+
+    public boolean isOpen() {
+        return getLocator().isVisible();
+    }
+
+    public void assertOpen() {
+        assertThat(getLocator()).isVisible();
+    }
+
+    public void assertClosed() {
+        assertThat(getLocator()).isHidden();
+    }
+
+    public Locator getContentLocator() {
+        return getLocator();
+    }
+}
